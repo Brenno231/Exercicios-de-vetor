88e880ad68f1f50b59d5e7cf732cc5e72b9a1f3a
Brnn231 <brennosoares665@gmail.com>
2026-05-13 10:32:07 -0300

    Correção de indentaçã

--- Primos.java
+++ Primos.java
@@ -1,28 +1,28 @@
 
 import java.util.Scanner;
 
-public class Primos{
+public class Primos {
     public static void main(String[] args) {
         int n[] = new int[10];
         Scanner ler = new Scanner(System.in);
-        for(int i = 0; i < n.length; i++){
-            n [i] = ler.nextInt();
+        for (int i = 0; i < n.length; i++) {
+            n[i] = ler.nextInt();
         }
-        for(int i = 0; i < n.length; i++){
+        for (int i = 0; i < n.length; i++) {
             double raiz = Math.sqrt(n[i]);
             int div = 2;
             boolean primo = true;
-            while(div <= raiz){
-                if(n [i] % div == 0){
+            while (div <= raiz) {
+                if (n[i] % div == 0) {
                     primo = false;
                     break;
                 }
                 ++div;
             }
-            if(primo && n [i] > 1){
-                System.out.printf("%d ",n[i]);
+            if (primo && n[i] > 1) {
+                System.out.printf("%d ", n[i]);
             }
-            
+
         }
         System.out.print("-> Esses números são primos\n");
     }
--- ValorInvertidoVetor.java
+++ ValorInvertidoVetor.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 public class ValorInvertidoVetor {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        int conjA [] = new int[5];
+        int conjA[] = new int[5];
         int invertido = 0, inversao = 0;
-        for(int i = 0; i < conjA.length; i++){
-            conjA [i] = ler.nextInt();
-            while(conjA[i] > 0){
+        for (int i = 0; i < conjA.length; i++) {
+            conjA[i] = ler.nextInt();
+            while (conjA[i] > 0) {
                 inversao = conjA[i] % 10; // pega o último dígito
                 invertido = (invertido * 10) + inversao; // coloca o último dígito em 1° e aumenta uma casa
-                conjA[i] /= 10; // divide por 10 para que o penúltimo número vire o último, ex: 12 -> 1 
+                conjA[i] /= 10; // divide por 10 para que o penúltimo número vire o último, ex: 12 -> 1
             }
             conjA[i] = invertido;
-            System.out.printf("%d ",conjA[i]);
+            System.out.printf("%d ", conjA[i]);
             invertido = 0;
-        }        
+        }
     }
 }
